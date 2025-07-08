6283748ff73157e7fd9f89b643a623ccc9e80fe9
MD-Kayesur <rmdkayesur@gmail.com>
2025-07-08 20:01:23 +0600

    first commit

--- src/Hooks/useAxious.jsx
+++ src/Hooks/useAxious.jsx
@@ -2,9 +2,9 @@ import axios from "axios";
 
  
  export const AxiousURL = axios.create({
-    baseURL:'http://localhost:4000',
-    // withCredentials:true,
-    // baseURL:'https://building-management-server-two.vercel.app',
+    // baseURL:'http://localhost:4000',
+    withCredentials:true,
+    baseURL:'https://building-management-server-two.vercel.app',
     timeout:5000
 })
 const useAxious = () => {
